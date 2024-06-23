8501bdec6980dcc5c203bae25e20bdd54eaa3a36
Jonathanbloo <132208363+Jonathanbloo@users.noreply.github.com>
2024-06-23 12:09:12 +0200

    Ajout fonction des caterogies et filtre par click

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,82 +1,71 @@
 
 
 /** Les Variables **/
+const gallery = document.querySelector(".gallery");
+const filterSection = document.querySelector('.filtre');
 
 
-/* Fonction qui appel et retourne le tableau works en json */
+/* Fonction qui appel et retourne le tableau works et categorie en json */
 async function getWorks(){
-    const reponse = await fetch("http://localhost:5678/api/works")
-    const projets = await reponse.json();
-
-worksView(projets);
+    const response = await fetch("http://localhost:5678/api/works")
+    return await response.json();
 }
 getWorks();
 
+async function getCategorys(){
+    const response = await fetch("http://localhost:5678/api/categories");
+    return await response.json();
+}
 
 /**  affichage du works **/
 
-async function worksView(){      /* creation d'une function qui affiche mes works */
+async function worksView(filterId = 0){      /* creation d'une function qui affiche mes works */
     const arrayworks = await getWorks();    /* variable qui appelle pour tableau works json */
-    arrayworks.forEach(element => {         /* foreach qui permet de rechercher mes elements dans mon tableau */
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = "";
+
+    // Filtrer les travaux en fonction de l'ID de la catégorie
+    const worksToDisplay = filterId === 0 ? arrayworks : arrayworks.filter(work => work.categoryId === filterId);
+
+
+    worksToDisplay.forEach(element => {         /* foreach qui permet de rechercher mes elements dans mon tableau */
     const galleryImage = document.createElement('img');   /* variable qui cree un element image */
     galleryImage.src = element.imageUrl;                    /* permet d'aller chercher l'image depuis l'objet */
     const galleryTitle = document.createElement('figurecaption'); /* variable qui cree un element figurecaption */
     galleryTitle.textContent = element.title;                 /* permet d'aller chercher le titre depuis l'objet */
-    const galleryCategoryId = document.createElement('figure'); /* variable qui cree un une balise figure */
-    galleryCategoryId.appendChild(galleryImage);  /* permet de mettre l'element image en enfant de figure */
-    galleryCategoryId.appendChild(galleryTitle);  /* permet de mettre l'element figurecaption en enfant de figure */
-    gallery.appendChild(galleryCategoryId);       /* permet de mettre l'element figure en enfant de la section gallery */
-;
+    const galleryFigure = document.createElement('figure'); /* variable qui cree un une balise figure */
+    galleryFigure.appendChild(galleryImage);  /* permet de mettre l'element image en enfant de figure */
+    galleryFigure.appendChild(galleryTitle);  /* permet de mettre l'element figurecaption en enfant de figure */
+    gallery.appendChild(galleryFigure);       /* permet de mettre l'element figure en enfant de la section gallery */
+
 })
 }
 worksView();
 
 
-/** Recuperation des categories pour notre filtre **/
+/** Recuperation des categories pour notre filtre et filtre par categorie **/
 
-async function getCategorys() { /* creation d'une function qui affiche les categories */
-    const reponse = await fetch("http://localhost:5678/api/categories");
-    const categorie = await reponse.json();
-    const gallery = document.querySelector('.gallery');
-    const Filtres = document.querySelector('.filtre');
-    const btnTous = document.createElement('button');
-    btnTous.innerText = "Tous";
-    Filtres.appendChild(btnTous);
-    btnTous.classList.add(".filtre__style")
-    btnTous.addEventListener("click", function(){
-        gallery.innerHTML="";
-        getWorks();
-    })
-    for (let filtres of categorie) {
-        const button = document.createElement('button');
-        button.innerText = filtres.name;
-        Filtres.appendChild(button);
-        button.classList.add(".filtre__style")
-        button.addEventListener("click", async function(){
-            const reponse = await fetch("http://localhost:5678/api/works");
-            const btnFiltres = await reponse.json();
-            
-            const filtre = btnFiltres.filter(function (btnFiltres){
-                return btnFiltres.categoryId === filtres.id;
-            })
-            const gallery = document.querySelector('.gallery');
-            gallery.innerHTML="";
-            worksView(filtre);
-        })
-    }
+async function displayFilters() {
+    const filters = await getCategorys();
+    filters.unshift({ id: 0, name: "Tous"}); //une catégorie spéciale avec id: 0 et name: "Tous" Ce filtre permet d'afficher tous les travaux sans filtrage.
 
-}   
-getCategorys();
+    filters.forEach(filter => {
+        const button = document.createElement("button"); // Pour chaque catégorie dans filters, on crée un bouton HTML.
+        button.textContent = filter.name; // Le texte du bouton (button.textContent) est le nom de la catégorie (filter.name).
+        filterSection.appendChild(button); // Le bouton est ajouté à une section de filtres (filterSection).
+        button.classList.add('filtre__style');  // ajout d'une classe CSS (filtre__style) au bouton pour le styliser.
+
+        button.addEventListener("click", async () => { //événement de clic est ajouté à chaque bouton.
+            await worksView(filter.id);  //appelle worksView(filter.id), une fonction (non montrée ici) qui affiche les travaux correspondant à l'ID de la catégorie (filter.id)
+            console.log(filters);
+        });
+        
+    });
+}
+displayFilters();
 
 
 
-
-/** Affichage de mes btn par catégories **/
-
-
-
-
-/** Ajout du click qui permet de filtrer nos catégories **/
 
 
 
